1f5d37d0d286e3dd83b39677f17849de8ae55143
t9murkab1ts <84805550+t9murkab1ts@users.noreply.github.com>
2022-03-25 21:33:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите размер массива:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа для сортировки:");
+            int[] arr = new int[N];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Sort(arr);
+            Console.WriteLine("После сортировки:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+
+        }
+        static int[] Sort(int[] mas)
+        {
+        int temp;
+        for (int i = 0; i < mas.Length; i++)
+        {
+            for (int j = i + 1; j < mas.Length; j++)
+            {
+                if (mas[i] > mas[j])
+                {
+                    temp = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = temp;
+                }
+            }
+        }
+        return mas;
+        }
+    }
+
+}
+
+
